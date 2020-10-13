dd2ee7d5723fa7dd853c573be8d180998ce5b0c6
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-10-12 22:12:12 -0600

    Made various changes to quality of site

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,21 +11,22 @@ const Projects = () => {
         <div id="projects-page">
             <div id="projects-title">
                 <h1>Projects</h1>
-                <h3>During my time at Lambda, I had plenty of oppotunities to work solo or with my fellow developers on projects. Here are a few of my favorites.</h3>
+                <h3>While getting my education at Lambda, I had many opportunities to work solo and with fellow developers on creative projects. Here are a few of my favorites.</h3>
             </div>
 
             <div id="project-card1">
-                <div className="project-content">
-                    <a className="project-title" href="https://www.c.ecosoap.dev/login" target="_blank" rel="noopener noreferrer">Eco-Soap Bank</a>
-                    <p className="project-bio">During the Labs portion of Lambda, I had the opportunity to work with the company, Eco-Soap Bank. Their needs included a portal for admins modify a list of records that would dynamically display on a map. This rather large task was settled on the shoulders of myself and 2 other web developers, one of which left a week in.</p>
-                    <img className="project-img" src={map1} alt="a map with markers put on it"/>
+                <div className="project-content1">
+                    <a className="project-title" href="https://www.c.ecosoap.dev/login" target="_blank" rel="noopener noreferrer">Eco-Soap Bank*</a>
+                    <p className="project-bio">During the Labs portion of Lambda, I had the opportunity to work with the company, Eco-Soap Bank. Their needs included a portal for admins to modify a list of records that would dynamically display on a map. This project was completed by myself and just 2 other web developers, one of which left after a week.</p>
+                    <p id="eco-soap-issue">*Due to an issue involving the stakeholder and mixed content, the site doesn't render data correctly. Follow <a href="https://docs.adobe.com/content/help/en/target/using/experiences/vec/troubleshoot-composer/mixed-content.html">this guide</a> to enable viewing.</p>
+                    <img className="project-img1" src={map1} alt="a map with markers put on it"/>
                 </div>
             </div>
 
             <div id="project-card2">
                 <div className="project-content">
                     <a className="project-title" href="https://theonering.vercel.app/" target="_blank" rel="noopener noreferrer">The One Ring Minecraft Server</a>
-                    <p className="project-bio">While still plowing through Lambda's curriculum, I was approached about creating a website for a hobby of mine: A Lord of the Rings Minecraft Server. It became a side-project of mine that really helped spark a greater love of coding, as I researched more cool utilities and API's I could use.</p>
+                    <p className="project-bio">While attending Lambda, I was approached about creating a website for a hobby of mine: A Lord of the Rings Minecraft Server. It became a side-project that really helped spark a greater love of coding, as I researched more cool utilities and API's I could use.</p>
                     <img className="project-img" src={lotr} alt="the landing page for The One Ring"/>
                 </div>
             </div>
@@ -41,15 +42,15 @@ const Projects = () => {
             <div id="project-card4">
                 <div className="project-content">
                     <a className="project-title" href="https://usemytechstuff2.now.sh/" target="_blank" rel="noopener noreferrer">Use-My-Techstuff</a>
-                    <p id="project-bio4">My first project I created at Lambda. While working on a larger team of web developers (2 HTML/CSS devs,3 React devs, and 1 NodeJS dev), it was my task to build a marketing page for our app with the assistance of one of my fellow developers. I developed the Home and Contact Us pages while his responsibility was the About and FAQ pages.</p>
+                    <p id="project-bio4">This was my first team project at Lambda. While working on a larger team of web developers (2 HTML/CSS devs, 3 React devs, and 1 backend dev), it was my task to build a marketing page for our app with the assistance of one of my fellow developers. I developed the Home and Contact Us pages while his responsibility was the About and FAQ pages.</p>
                     <img id="project-img4" src={umts} alt="landing page for Use-My-Techstuff"/>
                 </div>
             </div>
 
             <div id="bottom-card">
-                <h2 id="bottom-title">And many more!</h2>
-                <p id="bottom-bio">Interested in seeing the code for these projects? Head over to my github to see that and more!</p>
-                <a id="github-link" href="https://github.com/joshuabhorrocks" target="_blank" rel="noopener noreferrer">Click Here!</a>
+                <h2 id="bottom-title">More Information</h2>
+                <p id="bottom-bio">Interested in seeing the code for these projects? Head over to my github to see these and many more.</p>
+                <button id="github-link"><a href="https://github.com/joshuabhorrocks">Click Here</a></button>
             </div>
         </div>
     )
